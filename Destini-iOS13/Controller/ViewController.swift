ec02ceac052f77431e4e375a258a7d65dcfed012
erika-h7 <erika.herrera1@outlook.com>
2022-09-06 05:39:58 -0700

    The goal is to become familiar with MVC design pattern and thinking of the state of the app.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road"
+    let choice1 = "Take a left 👈"
+    let choice2 = "Take a right 👉"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyLabel.text = story0
+        
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+import Foundation
+
+struct Story {
+    let storyTitle: String
+    let choice1: String
+    let choice2: String
+    
+    init(title: String, choice1: String, choice2: String){
+        storyTitle = title
+        self.choice1 = choice1
+        self.choice2 =  choice2
+    }
+}
