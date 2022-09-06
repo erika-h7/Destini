d1663e195a51314f167b7408843d84c06988c0dc
erika-h7 <erika.herrera1@outlook.com>
2022-09-06 10:09:31 -0700

    Created a choose your own adventure game app.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,21 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let story0 = "You see a fork in the road"
-    let choice1 = "Take a left 👈"
-    let choice2 = "Take a right 👉"
+    // Story instance
+    var storyBrain = StoryBrain()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        updateUI()
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+        
     }
     
+    // Updating UI
+    func updateUI() {
+        
+        // Updates UI to the first question and first button options
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+
+    }
 }
 
